08e425e3f26e6c957e8370f1ae8be8c007293a2c
Imranalikm <imranalikm@gmail.com>
2024-02-23 11:38:15 +0530

    query service added

--- comments/index.js
+++ comments/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
-const cors = require('cors')
+const cors = require('cors');
+const axios = require('axios')
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -11,7 +12,7 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments',async  (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
 
@@ -20,9 +21,21 @@ app.post('/posts/:id/comments', (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
-
+  await axios.post('http://localhost:4005/events',{
+    type:'CommentCreated',
+    data:{
+      id: commentId, 
+      content,
+      postId:req.params.id
+    }
+  })
   res.status(201).send(comments);
 });
+
+app.post('/events',(req,res)=>{
+  console.log("event recieved",req.body.type)
+  res.send({});
+})
 
 app.listen(4001, () => {
   console.log('Listening on 4001');
